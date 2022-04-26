f31d38377bc7cb679ef9b2120af47fac3b54981a
Mia <mdrago2107@stu.d214.org>
2022-04-26 08:48:44 -0700

    Initial Commit

---
+++ GreatReads/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GreatReads
+//
+//  Created by Mia Drago on 4/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
